6ffea36a6d7984ef501f4dd7e7a0ce8980cac928
alex.barro <a.barro.sendros@gmail.com>
2025-03-02 22:35:52 +0100

    feat: implement PlaylistsService for searching playlists

---
+++ src/services/playlistsService.ts
@@ -0,0 +1,25 @@
+import { ResponseData } from '@/pages/api/playlists/search';
+import { IFetchService } from './fetchService';
+
+export type Playlist = {
+  name: string;
+  description?: string;
+  isPublic: boolean;
+};
+
+export interface IPlaylistsService {
+  searchPlaylists(_name: string, _limit: number): Promise<ResponseData>;
+}
+
+export class PlaylistsService implements IPlaylistsService {
+  private fetchService: IFetchService;
+
+  constructor(fetchService: IFetchService) {
+    this.fetchService = fetchService;
+  }
+
+  async searchPlaylists(name: string, limit: number): Promise<ResponseData> {
+    const url = `/api/playlists/search?name=${name}&limit=${limit}`;
+    return this.fetchService.fetchData<ResponseData>(url);
+  }
+}
